470218284ba44a0532e5b73ace7d89dc76959028
gvv <memovargasv@gmail.com>
2019-09-07 11:15:20 -0500

    con fns iniciales

--- src/App.js
+++ src/App.js
@@ -4,28 +4,53 @@ import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+// Notes Components
 import NotesForm from './NotesForm'
 
-function App() {
-  return (
-    <Fragment>
-      <Typography align='center' variant='h2' gutterBottom>
-        Hola Mundo!
-      </Typography>
-      <Grid container justify='center' spacing={2}>
-        <Grid item xs={4}>
-          {/* NotesList */}
-        </Grid>
-        <Grid item xs={8}>
-          <NotesForm />
-        </Grid>
-      </Grid>
 
-      <Fab color='primary' className='addIcon'>
-        <AddIcon />
-      </Fab>
-    </Fragment>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: '',
+      description: '',
+      notes: []
+    }
+  }
+
+  updateValue = e => {
+    console.log(e.target.value);
+    this.setState({
+      [e.target.name]: e.target.value
+      
+    })
+  };
+
+  render (){
+    console.log(this.state)
+    return (
+      <Fragment>
+        <Typography align='center' variant='h2' gutterBottom>
+          Hola Mundo!
+        </Typography>
+  
+        <Grid container justify='center' spacing={2}>
+          <Grid item xs={4}>
+            {/* NotesList */}
+          </Grid>
+          <Grid item xs={8}>
+            <NotesForm title={this.state.title} description={this.state.description} updateValue={this.updateValue}/>
+          </Grid>
+        </Grid>
+  
+        <Fab color='primary' className='addIcon'>
+          <AddIcon />
+        </Fab>
+      </Fragment>
+    );
+  }
+
+  
 }
 
 export default App;
--- src/NotesForm.js
+++ src/NotesForm.js
@@ -5,14 +5,14 @@ import Grid from '@material-ui/core/Grid';
 import Fab from '@material-ui/core/Fab';
 import Icon from '@material-ui/core/Icon';
 
-const NotesForm = () => {
+const NotesForm = (props) => {
     return(
         <Fragment>
             <Grid item xs={12}>
-                <TextField type='text' label='Titulo' margin='normal' fullWidth/>
+                <TextField name='title' type='text' label='Titulo' margin='normal' fullWidth onChange={props.updateValue} value={props.title}/>
             </Grid>
             <Grid item xs={12}>
-                <TextField multiline rows='4' margin='normal' fullWidth placeholder='Anote'/>
+                <TextField name='description' multiline rows='4' margin='normal' fullWidth placeholder='Anote' onChange={props.updateValue} value={props.description}/>
             </Grid>
 
             <Fab color='secondary' className='editIcon'>
